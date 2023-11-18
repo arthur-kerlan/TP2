0e4e7dbfdd89e2ef3c7607d9fbf54157c1be8f74
arthur-kerlan <arthur.mf.kerlan@gmail.com>
2023-11-18 20:39:34 -0300

    Infinite map added

--- mainGame.js
+++ mainGame.js
@@ -1,11 +1,12 @@
 var config = {
-    type: Phaser.AUTO,
-    width: 1580,
-    height: 200,
+    type: Phaser.CANVAS,
+    width: '100%',
+    height: 400,
+    backgroundColor: '#353535',
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 5000 },
             debug: false
         }
     },
@@ -20,6 +21,10 @@ var game =  new Phaser.Game(config);
 
 var player;
 
+var ground;
+
+var cursors;
+
 var static_ground;
 
 function preload(){
@@ -30,11 +35,26 @@ function preload(){
 
 function create(){
     
-    static_ground = this.physics.add.staticGroup();
+    //ground = this.physics.add.sprite(0, 368, 'ground').setOrigin(0,0);
+    ground = this.physics.add.group({
+        key: 'ground',
+        repeat: 2,
+        setXY: {x: 1200, y: 368, stepX: 2400}
+    });
 
-    static_ground.create(0, 172, 'ground').setOrigin(0, 0);
+    ground.children.iterate((child) =>{
+        child.setVelocityX(-300);
+    });
 
-    player = this.physics.add.sprite(100, 450, 'dino');
+    var ground_colisor = this.physics.add.staticSprite(0,420).setOrigin(0,0);
+    ground_colisor.body.width = 2400;
+
+
+    static_ground = this.physics.add.staticSprite('0%', 410).setOrigin(0,0);
+    static_ground.body.height = 1;
+    static_ground.body.width = 2400;
+
+    player = this.physics.add.sprite(100, 650, 'dino');
 
     player.setCollideWorldBounds(true);
 
@@ -51,20 +71,27 @@ function create(){
     });
 
     this.physics.add.collider(player, static_ground);
+    this.physics.add.collider(ground, ground_colisor);
 
     cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update(){
 
-    
+    ground.children.iterate((child) =>{
+        
+            if(child.body.x == -2400)
+                child.body.x = 1600;
+    });
 
     if (cursors.up.isDown && player.body.touching.down)
     {
         player.anims.play('stand');
-        player.setVelocityY(-330);
+        player.setVelocityY(-1500);
     }
+
     else if(player.body.touching.down){
+        
         player.anims.play('walking', 'stand');
     }
 }
