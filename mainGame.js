bfa459e7370c7879f748485859f3cc36e1ba57e7
arthur-kerlan <arthur.mf.kerlan@gmail.com>
2023-11-19 11:08:03 -0300

    test with cactus

--- mainGame.js
+++ mainGame.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.CANVAS,
     width: '100%',
-    height: 400,
+    height: '50%',
     backgroundColor: '#353535',
     physics: {
         default: 'arcade',
@@ -22,41 +22,49 @@ var game =  new Phaser.Game(config);
 var player;
 
 var ground;
+const groundWidth = 1200;
+const groundHeight = 24;
+var groundInitialVelocity = -300;
+var cactus1
 
 var cursors;
 
 var static_ground;
 
 function preload(){
+
+
     this.load.image('ground', 'imgs/gameFrames/ground.png');
     this.load.spritesheet('dino', 'imgs/gameFrames/2x-trex.png',{frameWidth: 88, frameHight: 94});
-    
+    this.load.image('cactus1','imgs/gameFrames/cactus_1.png');
 }
 
 function create(){
+
+    const alturaJogo = this.sys.canvas.height;
+    const larguraJogo = this.sys.canvas.width;
     
     //ground = this.physics.add.sprite(0, 368, 'ground').setOrigin(0,0);
     ground = this.physics.add.group({
         key: 'ground',
         repeat: 2,
-        setXY: {x: 1200, y: 368, stepX: 2400}
+        setXY: {x: 0, y: alturaJogo-10, stepX: groundWidth*2}
     });
 
     ground.children.iterate((child) =>{
-        child.setVelocityX(-300);
+        child.setVelocityX(groundInitialVelocity);
     });
 
-    var ground_colisor = this.physics.add.staticSprite(0,420).setOrigin(0,0);
-    ground_colisor.body.width = 2400;
+    var ground_colisor = this.physics.add.staticSprite(0,alturaJogo+20).setOrigin(0,0);
+    ground_colisor.body.width = groundWidth*2;
 
 
-    static_ground = this.physics.add.staticSprite('0%', 410).setOrigin(0,0);
+    static_ground = this.physics.add.staticSprite('0%', alturaJogo+10).setOrigin(0,0);
     static_ground.body.height = 1;
-    static_ground.body.width = 2400;
+    static_ground.body.width = groundWidth*2;
 
-    player = this.physics.add.sprite(100, 650, 'dino');
+    player = this.physics.add.sprite(50, alturaJogo-94, 'dino').setOrigin(0,0);
 
-    player.setCollideWorldBounds(true);
 
     this.anims.create({
         key: 'walking',
@@ -70,19 +78,29 @@ function create(){
         repeat: -1
     });
 
+    cactus1 = this.physics.add.sprite(larguraJogo, alturaJogo-100-18, 'cactus1').setOrigin(0,0);
+    cactus1.setVelocityX(groundInitialVelocity);
+
     this.physics.add.collider(player, static_ground);
     this.physics.add.collider(ground, ground_colisor);
+    this.physics.add.collider(cactus1, static_ground);
 
     cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update(){
 
+    const larguraJogo = this.sys.canvas.width;
+
     ground.children.iterate((child) =>{
         
-            if(child.body.x == -2400)
-                child.body.x = 1600;
+            if(child.body.x <= groundWidth*(-2))
+                child.body.x = larguraJogo;
+
     });
+
+    if(cactus1.body.x  <= -48)
+        cactus1.body.x = larguraJogo;
 
     if (cursors.up.isDown && player.body.touching.down)
     {
